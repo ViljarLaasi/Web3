04ae5e70fe8cd1c094fa429548b8479ba5fbb5e6
ViljarLaasi <viljarlaasi@gmail.com>
2017-03-17 13:43:01 +0200

    proov1

--- src/main/java/viljar/Rakendus.java
+++ src/main/java/viljar/Rakendus.java
@@ -7,24 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Rakendus {
+
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
         return "Ahoi!";
     }
-	@RequestMapping("/geomeetriline_keskmine")
-		String gk(String arv1, String arv2){
-			if(arv1==null){return "andmed puuduvad";}
-			int a1=Integer.parseInt(arv1);
-			int a2=Integer.parseInt(arv2);
-			return arv1+"ja"+arv2+"geomeetriline keskmine on " +a1*a2;
-		}
+
 	@RequestMapping("/tervita")
-				String gk(String arv1, String arv2){
-					if(arv1==null){return "andmed puuduvad";}
-					int a1=Integer.parseInt(arv1);
-					int a2=Integer.parseInt(arv2);
-					return arv1+"ja"+arv2+"geomeetriline keskmine on " +a1*a2;
-	    }
+	String tervitusfunktsioon3(String eesnimi){
+		return "Tere, "+eesnimi;
+	}
+
+    public static void main(String[] args) {
+		//System.getProperties().put("server.port", 4200);
+        SpringApplication.run(Rakendus.class, args);
+    }
+}
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
 
 
     public static void main(String[] args) {
